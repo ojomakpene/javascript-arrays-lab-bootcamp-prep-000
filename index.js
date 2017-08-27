55416fa629ed1866328e16239ba9ae633286dfad
Sani Abubakar <egbaojo@gmail.com>
2017-08-27 07:42:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+var name = "Ralph"
+function kittens() {
+  return kittens
+}
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
